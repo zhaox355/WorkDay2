1bd1eeb983bf5f8dcddfb8740bf0230bc82d1166
idsc3101 <idsc3101@gmail.com>
2013-09-16 15:24:24 -0500

    Anna Zhao

--- FirstName/FirstName/Form1.Designer.vb
+++ FirstName/FirstName/Form1.Designer.vb
@@ -25,61 +25,67 @@ Partial Class Form1
         Me.txtFullName = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnFirstName = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'txtFullName
         '
-        Me.txtFullName.Location = New System.Drawing.Point(206, 43)
+        Me.txtFullName.Location = New System.Drawing.Point(154, 35)
+        Me.txtFullName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtFullName.Name = "txtFullName"
-        Me.txtFullName.Size = New System.Drawing.Size(203, 22)
+        Me.txtFullName.Size = New System.Drawing.Size(153, 20)
         Me.txtFullName.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 46)
+        Me.Label1.Location = New System.Drawing.Point(9, 37)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(188, 17)
+        Me.Label1.Size = New System.Drawing.Size(141, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Please Type Your Full Name"
         '
         'btnFirstName
         '
-        Me.btnFirstName.Location = New System.Drawing.Point(145, 83)
+        Me.btnFirstName.Location = New System.Drawing.Point(109, 67)
+        Me.btnFirstName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnFirstName.Name = "btnFirstName"
-        Me.btnFirstName.Size = New System.Drawing.Size(151, 26)
+        Me.btnFirstName.Size = New System.Drawing.Size(113, 21)
         Me.btnFirstName.TabIndex = 2
         Me.btnFirstName.Text = "Extract First Name"
         Me.btnFirstName.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtFirstName
         '
-        Me.TextBox1.Location = New System.Drawing.Point(206, 140)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(203, 22)
-        Me.TextBox1.TabIndex = 3
+        Me.txtFirstName.Location = New System.Drawing.Point(154, 114)
+        Me.txtFirstName.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(153, 20)
+        Me.txtFirstName.TabIndex = 3
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(76, 143)
+        Me.Label2.Location = New System.Drawing.Point(57, 116)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(124, 17)
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Your First Name is"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(455, 218)
+        Me.ClientSize = New System.Drawing.Size(341, 177)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtFirstName)
         Me.Controls.Add(Me.btnFirstName)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtFullName)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "FirstName"
         Me.ResumeLayout(False)
@@ -89,7 +95,7 @@ Partial Class Form1
     Friend WithEvents txtFullName As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents btnFirstName As System.Windows.Forms.Button
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
     Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,19 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim fullname As String = txtFullName.Text
+        'read the input from the user
+        'display the result to thr user
 
+        Dim fullnameTrimmed As String
+        fullnameTrimmed = fullname.Trim()
+
+        Dim idxSpace As Integer
+        idxSpace = fullnameTrimmed.IndexOf(" ")
+        'extract the forst name
+        Dim firstname As String
+
+        firstname = fullnameTrimmed.Substring(0, idxSpace)
+        txtFirstName.Text = firstname
     End Sub
 End Class
